edb8034f6fea3fd8bba557f6f00ec8b875e39275
ridwanal24 <ridwanalakhyar1999@gmail.com>
2021-02-12 19:54:13 +0700

    adding font awesome

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <title>Toko Kocheng Chan | Home</title>
+</head>
+
+<body>
+    <!-- SESSION START -->
+    <?php
+    session_start();
+    // login check
+    ?>
+    <!-- SESSION END -->
+
+    <!-- navbar start -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a href="index.php" class="navbar-brand">TK-CHAN</a>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a href="index.php" class="nav-item nav-link">Home</a>
+                <a href="index.php" class="nav-item nav-link">Category</a>
+
+            </div>
+        </div>
+    </nav>
+    <!-- navbar end -->
+
+    <!-- content start -->
+    <div class="container-fluid">
+
+    </div>
+    <!-- content end -->
+
+    <!-- footer start -->
+    <div class="bg-black">
+        hhs
+    </div>
+    <!-- footer end -->
+
+    <!-- script -->
+</body>
+
+</html>
